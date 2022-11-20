6d513d0fc9fc4497f31eb8c73d6709de5171ba99
Uttam Marandi <uttammarandi007@gmail.com>
2022-11-20 20:03:58 +0530

    feat : update todos

--- packages/server/routers/todo.ts
+++ packages/server/routers/todo.ts
@@ -1,8 +1,9 @@
 import { t } from "../config/trpc";
 import { z } from "zod";
 import { todoSchema, TodoType } from "../models/todo";
+import { TRPCError } from "@trpc/server";
 
-const todos: TodoType[] = [];
+let todos: TodoType[] = [];
 
 export const todoRouter = t.router({
   all: t.procedure.query(() => {
@@ -11,6 +12,30 @@ export const todoRouter = t.router({
   add: t.procedure.input(todoSchema).mutation(({ input }) => {
     todos.push(input);
     return todos;
+  }),
+  delete: t.procedure.input(z.number()).mutation(({ input }) => {
+    const filteredTodos = todos.filter((todo) => todo.id !== input);
+    todos = [...filteredTodos];
+    return todos;
+  }),
+  update: t.procedure.input(todoSchema.partial()).mutation(({ input }) => {
+    // .partial() makes all the field optional
+    const index = todos.findIndex((todo) => todo.id === input.id);
+    const todo = todos?.[index];
+
+    if (!todo) {
+      throw new TRPCError({
+        code: "NOT_FOUND",
+        message: "Given id does not exist",
+      });
+    }
+
+    todos[index] = {
+      ...todo,
+      ...input, // overwrite with provided input fields
+    };
+
+    return todos[index];
   }),
 });
 // This input() method takes an input validation schema that defines what our incoming request body should look like.
